ebab95f6c5635d1b459db43dd97b98d1a5bcf473
Haidyn <haidyn1988@gmail.com>
2017-11-06 22:14:00 +1000

    Chore: add moving vehicle in simulator code

--- ros/src/twist_controller/pid.py
+++ ros/src/twist_controller/pid.py
@@ -4,13 +4,14 @@ MAX_NUM = float('inf')
 
 
 class PID(object):
-    def __init__(self, kp, ki, kd, mn=MIN_NUM, mx=MAX_NUM):
+    def __init__(self, kp, ki, kd, mn=MIN_NUM, mx=MAX_NUM, min_i=-50., max_i=50.):
         self.kp = kp
         self.ki = ki
         self.kd = kd
         self.min = mn
         self.max = mx
-
+        self.min_i = min_i
+        self.max_i = max_i
         self.int_val = self.last_int_val = self.last_error = 0.
 
     def reset(self):
@@ -20,10 +21,11 @@ class PID(object):
     def step(self, error, sample_time):
         self.last_int_val = self.int_val
 
-        integral = self.int_val + error * sample_time;
-        derivative = (error - self.last_error) / sample_time;
+        # clip the integral sum to prevent excessive integral error and gain
+        integral = max(self.min_i, min(self.int_val + error * sample_time, self.max_i))
+        derivative = (error - self.last_error) / sample_time
 
-        y = self.kp * error + self.ki * self.int_val + self.kd * derivative;
+        y = self.kp * error + self.ki * self.int_val + self.kd * derivative
         val = max(self.min, min(y, self.max))
 
         if val > self.max:
@@ -34,4 +36,7 @@ class PID(object):
             self.int_val = integral
         self.last_error = error
 
+        print("error: ", error)
+        print("d_error: ", derivative)
+        print("i_error: ", self.int_val)
         return val
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,14 +1,113 @@
+from pid import PID
+from lowpass import *
+from yaw_controller import YawController
+from math import *
 
+##
+#    Throttle PID
+##
+Kp_v = 2.5      #0.03      # 0.08      # 0.05
+Kd_v = 0.0006   #0.01      # 0.02      # 0.02
+Ki_v = 0.02     #.002     # 0.005     # 0.001
+
+##
+#    Steering PID
+##
+Kp_s = 0.05
+Kd_s = 0.02
+Ki_s = 0.001
+
+
+##
+#    Vehicle throttle control setting
+## 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
+MIN_THROTTLE = 0.0
+MAX_THROTTLE = 1.0
 
+MIN_INTEGRAL = -30.0 # this will max the integral error to only contribute 0.6 to the throttle
+MAX_INTEGRAL = 30.0
+
+
+
+# mynote: implement a feedback controller from pid,low pass (both for acceleration), yaw controller (steering)
 
 class Controller(object):
     def __init__(self, *args, **kwargs):
-        # TODO: Implement
-        pass
+        self.vehicle_cfg = args[0]
+        self.target_v = 0.0
+        self.target_w = 0.0
+        self.current_v = 0.0
+        self.dbw_status = False
+        self.throttle = PID(Kp_v, Ki_v, Kd_v, mn=MIN_THROTTLE, mx=MAX_THROTTLE, min_i=MIN_INTEGRAL, max_i=MAX_INTEGRAL)
+        self.error_v  = 0.0
+        self.wheel_base    = self.vehicle_cfg['wheel_base']
+        self.steer_ratio   = self.vehicle_cfg['steer_ratio']
+        self.min_speed     = 0.0
+        self.max_lat_accel = self.vehicle_cfg['max_lat_accel']
+        self.max_steer_angle = self.vehicle_cfg['max_steer_angle']
+        self.vehicle_mass  = self.vehicle_cfg['vehicle_mass']
+        self.wheel_radius  = self.vehicle_cfg['wheel_radius']
+        self.steering = YawController(self.wheel_base, self.steer_ratio, self.min_speed, self.max_lat_accel, self.max_steer_angle)
+        
 
     def control(self, *args, **kwargs):
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
-        return 1., 0., 0.
+        # Change the arg, kwarg list to suit your needs (args: target_v,target_w,current_v,dbw_status,dt)
+        # Feedback controls: pid/lowpass (throttle) and yaw controller (steering)
+        # Throttle := [0,1], Brake := N*m, Steering := Radian
+        
+        # velocity args
+        target_v   = args[0]#current_velocity.twist.linear
+        target_w   = args[1]#current_velocity.twist.angular
+        current_v  = args[2]#twist_cmd.twist.linear
+        dbw_status = args[3]#dbw
+        dt         = args[4]#sample time dt
+        # linear speed error in x
+        v_current = abs(current_v.x)
+        v_target  = abs(target_v.x)
+        v_error   = v_target - v_current
+
+        # Throttle control
+        throttle_cmd = self.throttle.step(v_error, dt)
+        
+        # Steering angle- steering output proportional to (v_current/v_target)
+        #               - something fishy about this yaw control scheme
+        #               - what happen if v_current > v_target much a lot? The steering might spin out of control!
+        #               - so we need to make sure the speed doesn't go pass the limit for too long & lowpass filter also help
+        w_target = target_w.z
+        steering_cmd = self.steering.get_steering(v_target, w_target, v_current)
+        steering_cmd = (steering_cmd+pi)%(2*pi) - pi
+        
+        print("target v.x: ", target_v.x)
+        print("target v.y: ", target_v.y)
+        print("target v.z: ", target_v.z)
+        print("target w.x: ", target_w.x)
+        print("target w.y: ", target_w.y)
+        print("target w.z: ", target_w.z)
+        
+        print("current v.x: ", current_v.x)
+        print("current v.y: ", current_v.y)
+        print("current v.z: ", current_v.z)
+        
+        # NOTE: Just to get thing going only:need to fine tune this
+        #       Or use different approach altogether
+        # Brake control                                 
+        d_speed = v_target - v_current
+        print("error v.x: ", d_speed)
+
+        brake_cmd = 0.0
+        d_t = 2.0   # desire braking time: reduce to the target speed within d_t
+        # over the limit, then we need to apply brake
+        # allow a small margin of error before applying the brakes
+        if(d_speed < -0.5):
+            # d_speed is negative so need to reverse it to positive
+            brake_cmd = -self.vehicle_mass*self.wheel_radius*d_speed/d_t
+        
+        
+        print("throttle: ", throttle_cmd)
+        print("brake: ", brake_cmd)
+        print("steering: ", steering_cmd)
+        
+        # Return throttle, brake, steer in this order
+        return throttle_cmd, brake_cmd, steering_cmd
